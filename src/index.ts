6e2896d4e1691bd7f5e45c551e636ca340108bfe
sdonich <sdonich@gmail.com>
2019-10-14 21:41:51 +0200

    Update User.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 import { User } from './models/Users';
 
-const user = new User({ name: 'newUser1', age: 17 });
+const user = new User({ id: 5, name: 'newer name', age: 999 });
 
+user.on('save', () => {
+  console.log(user);
+
+})
+
+user.save();
