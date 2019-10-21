f3047a4834513e1b9b93b28032e72e55273b44e0
sdonich <sdonich@gmail.com>
2019-10-21 20:55:38 +0200

    Add UserEdit

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import { UserForm } from './views/UserForm';
+import { UserEdit } from './views/UserEdit';
 import { User } from './models/User';
 
 const user = User.buildUser({ name: 'John', age: 200 })
 const root = document.getElementById('root');
 
 if (root) {
-  const userForm = new UserForm(
+  const userEdit = new UserEdit(
     root,
     user
   );
 
-  userForm.render();
+  userEdit.render();
 } else {
   throw new Error('Root element not found');
 }
