077f716fe0c625bcab424f224bffec534b76af81
sdonich <sdonich@gmail.com>
2019-10-21 17:05:29 +0200

    Add View.ts

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,15 +1,6 @@
-import { User } from '../models/User';
+import { View } from './View';
 
-export class UserForm {
-  constructor(public parent: Element, public model: User) {
-    this.bindModel();
-  }
-
-  bindModel(): void {
-    this.model.on('change', () => {
-      this.render();
-    });
-  }
+export class UserForm extends View {
 
   eventsMap(): { [key: string]: () => void } {
     return {
@@ -24,7 +15,7 @@ export class UserForm {
 
   onSetNameClick = (): void => {
     const input = this.parent.querySelector('input');
-    
+
     if (input) {
       const name = input.value;
       this.model.set({ name });
@@ -42,26 +33,5 @@ export class UserForm {
         <button class="set-age">Set Random Age</button>
       </div>
     `;
-  }
-
-  bindEvents(fragment: DocumentFragment): void {
-    const eventsMap = this.eventsMap();
-    for (let eventKey in eventsMap) {
-      const [eventName, selector] = eventKey.split(':');
-
-      fragment.querySelectorAll(selector).forEach(element => {
-        element.addEventListener(eventName, eventsMap[eventKey]);
-      })
-    }
-  }
-
-  render(): void {
-    this.parent.innerHTML = '';
-
-    const templateElement = document.createElement('template');
-    templateElement.innerHTML = this.template();
-    this.bindEvents(templateElement.content);
-
-    this.parent.append(templateElement.content);
   }
 }
